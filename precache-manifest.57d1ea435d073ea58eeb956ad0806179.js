117f08150d9cdd3795edda37f94111a4f8ca5fe0
Chase Kirby <chase12313@gmail.com>
2020-08-09 20:42:31 -0400

    Updates

---
+++ precache-manifest.57d1ea435d073ea58eeb956ad0806179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335cb0a002071aa9182600faca30a593",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "1aac84b3798cf39180d6",
+    "url": "/React/static/css/main.7307b930.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/React/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aac84b3798cf39180d6",
+    "url": "/React/static/js/main.127115d8.chunk.js"
+  },
+  {
+    "revision": "79458da80355d5a677b7",
+    "url": "/React/static/js/runtime-main.1a456d4b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.cd2ecdadcb6fb69e0fac182c9b60a345.js"
+  "/React/precache-manifest.57d1ea435d073ea58eeb956ad0806179.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
